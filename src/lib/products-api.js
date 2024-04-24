a94ae2fde4afce1a083bc09a3a4ef8455b152546
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-04-24 17:57:16 +0600

    issues solved

--- src/lib/products-api.js
+++ src/lib/products-api.js
@@ -1,19 +1,23 @@
-const API_URL = "https://fakestoreapi.com";
-
 export async function getProducts() {
-  const response = await fetch(`${API_URL}/products?limit=7`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_RESOURCES_ADDRESS}/products?limit=7`
+  );
   const products = await response.json();
   return products;
 }
 
 export async function getCategories() {
-  const response = await fetch(`${API_URL}/products/categories`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_RESOURCES_ADDRESS}/products/categories`
+  );
   const categories = await response.json();
   return categories;
 }
 
 export async function getProductsByCategory(category) {
-  const response = await fetch(`${API_URL}/products/category/${category}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_RESOURCES_ADDRESS}/products/category/${category}`
+  );
   const products = await response.json();
   return products;
 }
